ffc475826a93e1c8fa2bbf480662a6b0415b5c4f
SapTIBI <mysamuraicode@gmail.com>
2024-02-04 22:04:15 +0300

    Refactor API functions
    Added more detailed response statuses information: codes and messages.

--- backend/api.py
+++ backend/api.py
@@ -1,14 +1,14 @@
 from datetime import datetime, timedelta
+from functools import wraps
 
 import jwt
-
-from functools import wraps
 from flask import request, jsonify, make_response
 from flask.blueprints import Blueprint
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from database import database_api
 from config import SECRET_KEY
+
 
 api_v1 = Blueprint("api", __name__, url_prefix="/api/v1")
 
@@ -18,16 +18,16 @@ def token_required(api_function):
     def decorated(*args, **kwargs):
         authorization_header = request.headers.get('Authorization')
         if not authorization_header:
-            return jsonify({"message": 'Authentication failed!'})
-        auth_type, jwt_token = authorization_header.split(' ')
+            return jsonify({"message": 'Authentication failed!'}), 401
         try:
+            auth_type, jwt_token = authorization_header.split(' ')
             data = jwt.decode(jwt=jwt_token, key=SECRET_KEY, algorithms=["HS256"])
             account_id = data['id']
             return api_function(account_id, *args, **kwargs)
         except jwt.ExpiredSignatureError:
-            return jsonify({"message": 'Timeout data!'})
+            return jsonify({"message": 'Token has expired'}), 401   
         except jwt.InvalidTokenError:
-            return jsonify({"message": 'Incorrect authentication data!'})
+            return jsonify({"message": 'Invalid token'}), 401
     return decorated
 
 
@@ -35,8 +35,7 @@ def token_required(api_function):
 @token_required
 def testing_api(account_id):
     account = database_api.get_account_by_id(account_id)
-    response = make_response(jsonify({"account_id": account_id, 'account_name': account['account_name']}), 200)
-    return response
+    return jsonify({"account_id": account_id, 'account_name': account.get('account_name')}), 200
 
 
 @api_v1.route('/auth/login/', methods=['POST'])
@@ -45,38 +44,43 @@ def auth_login_new_account():
     name = data.get('account_name')
     login = data.get('account_login')
     password = data.get('account_password')
+    if not all((name, login, password)):
+        return jsonify({"message": 'Bad data!'}), 400
     hash_password = generate_password_hash(password)
     account_id = database_api.login_new_account(name, login, hash_password)
     if account_id:
-        encoded_jwt = jwt.encode(payload={"id": account_id, 'exp': datetime.utcnow() + timedelta(days=7)}, key=SECRET_KEY, algorithm="HS256")
-        response = make_response(jsonify({"message": 'Account was created!'}), 200)
+        expiration_time = datetime.utcnow() + timedelta(days=7)
+        encoded_jwt = jwt.encode(payload={"id": account_id, 'exp': expiration_time}, key=SECRET_KEY, algorithm="HS256")
+        response = jsonify({"message": 'Account was created!'})
         response.headers['Authorization'] = 'Bearer ' + encoded_jwt
-        print('AccountToken:' + encoded_jwt, f'Длина:{len(encoded_jwt)}')
-        return response
-    return make_response(jsonify({"message": 'Bad data!'}), 404)
+        return response, 201
+    return jsonify({"message": 'Failed to create account!'}), 409
+
 
 @api_v1.route('/auth/login/refresh', methods=['POST'])
 def auth_login_refresh():
     data = request.get_json()
     login = data.get('account_login')
     password = data.get('account_password')
+    if not all((login, password)):
+        return jsonify({"message": 'Bad data!'}), 400
     account = database_api.get_account_by_login(login)
-    if not account:
-        return jsonify({"message": 'Bad data!'})
-    if not check_password_hash(account.get('account_password'), password):
-        return jsonify({"message": 'Bad data!'})
+    if not account or not check_password_hash(account.get('account_password'), password):
+        return jsonify({"message": 'Bad data!'}), 400
     account_id = account.get('account_id')
-    encoded_jwt = jwt.encode(payload={"id": account_id, 'exp': datetime.utcnow() + timedelta(days=7)}, key=SECRET_KEY, algorithm="HS256")
-    response = make_response(jsonify({"message": 'Successful authentication!'}), 200)
+    expiration_time = datetime.utcnow() + timedelta(days=7)
+    encoded_jwt = jwt.encode(payload={"id": account_id, 'exp': expiration_time}, key=SECRET_KEY, algorithm="HS256")
+    response = jsonify({"message": 'Successful authentication!'})
     response.headers['Authorization'] = 'Bearer ' + encoded_jwt
-    return response
+    return response, 200
     
 
 @api_v1.route('/auth/logout/', methods=['POST'])
 @token_required
 def logout_account(account_id):
-    encoded_jwt = jwt.encode(payload={"id": account_id, 'exp': datetime.utcnow()}, key=SECRET_KEY, algorithm="HS256")
-    response = make_response(jsonify({"message": 'Successful logout!'}), 200)
+    expiration_time = datetime.utcnow()
+    encoded_jwt = jwt.encode(payload={"id": account_id, 'exp': expiration_time}, key=SECRET_KEY, algorithm="HS256")
+    response = jsonify({"message": 'Successful logout!'})
     response.headers['Authorization'] = 'Bearer ' + encoded_jwt
-    return response
+    return response, 200
 
